958d8f6888822be5fccac77084d546ca82f14bd8
mffree <matthewfreeman63@gmail.com>
2025-03-05 19:00:38 +0000

    devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "boards", :action => "index" })
+  # get("/", { :controller => "boards", :action => "index" })
+
+  root to: "home#index" # ADDED
 
   # Routes for the Post resource:
 
